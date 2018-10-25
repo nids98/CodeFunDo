8b5e737bdc7767d1feb34f494e8ec11ad1e5a8f8
Nidhi Shetty <nidhi.s1@learner.manipal.edu>
2018-10-25 16:02:40 +0530

    Update main.py

--- Succour/main.py
+++ Succour/main.py
@@ -59,7 +59,7 @@ city='Udupi'
 def index():
     with open('tweets.txt', 'r') as f:
         alltext = f.read()
-	tweet_list = alltext.split(',,,')
+        tweet_list = alltext.split(',,,')
     with open('retweetcount.txt', 'r') as f:
     	alltext = f.read()
     	retweet_count = alltext.split(',,,')
